b8f7e96271073e578e90cac9eb06f152ac182af1
schnlohani <schnlohani>
2022-03-13 16:34:37 +0530

    formaating

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart} from '@fortawesome/free-solid-svg-icons';
-import { faReact} from '@fortawesome/free-brands-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
 
 export const Footer = () => {
     return <footer className="flex flex-col bg-display-100 p-8 z-10 shadow-inner">
@@ -9,7 +9,7 @@ export const Footer = () => {
             {`Designed & Developed by Sachin Lohani`}
         </div>
         <div className="mx-auto">
-            <span> Built with <FontAwesomeIcon icon={faHeart}/> using  <FontAwesomeIcon icon={faReact}/> , Next.js and Tailwind CSS </span>
+            <span> Built with <FontAwesomeIcon icon={faHeart} /> using  <FontAwesomeIcon icon={faReact} /> , Next.js and Tailwind CSS </span>
         </div>
     </footer>
 }
